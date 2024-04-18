ec3b8d1124bcb554d12df2fe51cae8f0de752e8c
Matheus Buschermoehle <matheusbuschermoehle@gmail.com>
2024-04-17 21:05:45 -0300

    feat(refresh)

--- modelo_1/ml_esp_esboco.py
+++ modelo_1/ml_esp_esboco.py
@@ -1,12 +1,10 @@
 import numpy as np
 import tensorflow as tf
-'''
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
 from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras.optimizers import Adam, SGD, RMSprop
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-'''
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
